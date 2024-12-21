780f128c82242c9811142d231d8603cb2ed5db8b
Anna-fog <anna49.fog@yandex.ru>
2024-12-21 13:32:11 +0100

    Updated  routes

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,10 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
+      path: '/',
+      redirect: '/game',
+    },
+    {
       path: '/game',
       name: 'game',
       component: GameView
@@ -12,9 +16,6 @@ const router = createRouter({
     {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
     }
   ]
